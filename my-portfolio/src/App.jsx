f151cc2eb2a984b763ff32e391a2d1661ea02869
Ed <edrianhernandez07@gmail.com>
2025-06-14 23:18:26 +0800

    Navbar Initial

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
 
-function App() {
-  
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    </>
+  )
 }
 
 export default App
