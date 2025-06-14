4f96e94d397aa190934e0769478e8f5d2bd3ab02
Ed <edrianhernandez07@gmail.com>
2025-06-14 21:42:57 +0800

    Beta Test

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React</h1>// This is a simple React application using Vite as the build tool.
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
