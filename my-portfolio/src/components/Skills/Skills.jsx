28ba2a0634928037aa7a54bb3bdb3827082b4173
Ed <edrianhernandez07@gmail.com>
2025-06-15 21:07:24 +0800

    Skills Section Optimize

--- my-portfolio/src/components/Skills/Skills.jsx
+++ my-portfolio/src/components/Skills/Skills.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './Skills.css'
+import { SKILLS } from '../../utils/data'
+import SkillCard from './SkillCard/SkillCard.jsx'
+import SkillsInfoCard from './SkillsInfoCard/SkillsInfoCard.jsx'
 
 const Skills = () => {
+
+  const [selectedSkill, setSelectedSkill] = React.useState(SKILLS[0])
+
+  const handleSelectedSkill = (data) => {
+    setSelectedSkill(data)
+  }
+
   return (
-    <div>Skills</div>
+    <section className="skills-container">
+      <h5>Technical Proficiency</h5>
+
+      <div className="skills-content">
+        <div className="skills">
+          {SKILLS.map((item) => (
+            <SkillCard
+              key={item.title}
+              iconURL={item.icon}
+              title={item.title}
+              isActive={selectedSkill.title === item.title}
+              onClick={() => {
+                handleSelectedSkill(item);
+              }}
+            />
+          ))}
+        </div>
+
+        <div className="skills-info">
+          <SkillsInfoCard
+            heading={selectedSkill.title}
+            skills={selectedSkill.skills}
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ my-portfolio/src/utils/data.js
@@ -0,0 +1,41 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        icon: "./assets/icons8-ui-64.png",
+        skills: [
+            { skill: "HTML5", percentage: "80%"},
+            { skill: "CSS3 (Tailwind)", percentage: "70%" },
+            { skill: "JavaScript", percentage: "60%" },
+            { skill: "ReactJS", percentage: "50%" },
+        ],
+    },
+    {
+        title: "Backend",
+        icon: "./assets/icons8-backend-64.png",
+        skills: [
+            { skill: "MongoDB", percentage: "50%" },
+            { skill: "MySQL", percentage: "40%" },
+        ],
+    },
+    {
+        title: "Tools",
+        icon: "./assets/icons8-tools-64.png",
+        skills: [
+            { skill: "Git & Github", percentage: "75%" },
+            { skill: "Visual Studio Code", percentage: "75%" },
+            { skill: "Figma", percentage: "90%" },
+            { skill: "Responsive Design", percentage: "70%" },
+        ],
+    },
+    {
+        title: "Soft Skills",
+        icon: "./assets/icons8-communication-skills-50.png",
+        skills: [
+            { skill: "Problem Solving", percentage: "80%" },
+            { skill: "Teamwork", percentage: "90%" },
+            { skill: "Communication", percentage: "85%" },
+            { skill: "Attention to Details", percentage: "85%" },
+        ],
+    }
+];
+    
